17bc0f19251f7c3d55870ee812cc812ed2e215a9
Victor Martins <victor.brengenha@gmail.com>
2023-03-18 22:32:46 -0300

    arquivos

---
+++ content/cont_sobre.php
@@ -0,0 +1,315 @@
+<main id="main">
+
+    <div class="breadcrumbs">
+        <div class="page-header d-flex align-items-center" style="background-image: url('<?php echo BASEURL ?>img/bg-04.jpg');">
+            <div class="container position-relative">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-lg-6 text-center">
+                        <h2 id="titulo">Sobre</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav>
+            <div class="container">
+                <ol>
+                    <li><a href="<?php echo BASEURL ?>index.php">Home</a></li>
+                    <li>Sobre</li>
+                </ol>
+            </div>
+        </nav>
+    </div>
+
+
+    <section id="about" class="about">
+        <div class="container" data-aos="fade-up">
+
+            <div class="row gy-4">
+                <div class="col-lg-6 position-relative align-self-start order-lg-last order-first">
+                    <img src="<?php echo BASEURL ?>img/mvv.jpg" class="img-fluid" alt="" style="border-radius: 2px;">
+                </div>
+                <div class="col-lg-6 content order-last  order-lg-first">
+                    <h3>Sobre Nós</h3>
+                    <p>
+                        Localizada na cidade de Extrema - MG, a B&R Engenharia e Tecnologia atua com o Desenvolvimento
+                        de Sistemas, Websites e Suporte em TI na área de Tecnologia, e com Laudos, Projetos, Inspeções e
+                        Treinamentos nas áreas de Engenharia Elétrica, Mecânica e Segurança do Trabalho.
+                    </p>
+                    <ul>
+                        <li data-aos="fade-up" data-aos-delay="100">
+                            <i class="bi bi-check-all"></i>
+                            <div>
+                                <h5>Qualidade e Foco</h5>
+                                <p>Focamos na Qualidade Técnica dos serviços Oferecidos.</p>
+                            </div>
+                        </li>
+                        <li data-aos="fade-up" data-aos-delay="200">
+                            <i class="bi bi-award"></i>
+                            <div>
+                                <h5>Pilares e Valores</h5>
+                                <p>A B&R tem como pilares a Ética, o Conhecimento, a Inovação e a Satisfação de seus
+                                    clientes.</p>
+                            </div>
+                        </li>
+                        <li data-aos="fade-up" data-aos-delay="300">
+                            <i class="bi bi-braces"></i>
+                            <div>
+                                <h5>Profissionais e Capacitação</h5>
+                                <p>Contamos com profissionais Capacitados e Atualizados diante das mais diversas
+                                    tecnologias do mercado, oferecendo Segurança e Praticidade.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <section id="team" class="team pt-0">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-header">
+                <span>Nosso Time</span>
+                <h2>Nosso Time</h2>
+
+            </div>
+
+            <div class="row" data-aos="fade-up" data-aos-delay="100">
+
+                <div class="col-lg-4 col-md-6 d-flex">
+                    <div class="member">
+                        <img src="<?php echo BASEURL ?>img/p2.jpg" class="img-fluid" alt="">
+                        <div class="member-content">
+                            <h4>Rodrigo Otávio Rocha Cardoso</h4>
+                            <span>Sócio Proprietário</span>
+                            <p>Engenheiro Eletricista e Engenheiro de Segurança do Trabalho</p>
+                            <p align="justify">Mestre em Engenharia pelo Inatel, Pós Graduado em Engenharia de Segurança
+                                do trabalho na Cruzeiro do Sul, Engenheiro Eletricista pelo Inatel, Técnico em
+                                Eletrônica pela ETE “FMC”, sócio proprietário da B&R, atua como coordenador dos cursos
+                                de Engenharia de Produção e Análise e Desenvolvimento de Sistemas na FAEX, professor
+                                titular de disciplinas na área de engenharia e tecnologia (Ex: Instalações Elétricas) na
+                                FAEX, atuou como gerente de desenvolvimento de produtos por 4 anos e engenheiro de
+                                projetos por 6 anos na Linear Equipamentos Eletrônicos.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex">
+                    <div class="member">
+                        <img src="<?php echo BASEURL ?>img/p2.jpg" class="img-fluid" alt="">
+                        <div class="member-content">
+                            <h4>Bruno Gomes da Silva</h4>
+                            <span>Sócio Proprietário</span>
+                            <p>Engenheiro Mecânico e Engenheiro de Segurança do Trabalho</p>
+                            <p align="justify">Pós Graduado em Engenharia de Segurança do Trabalho na Universidade
+                                Cruzeiro do Sul, formado em Engenharia Mecânica pela Universidade São Francisco, sócio
+                                proprietário da B&R, atua como Engenheiro Mecânico. Possui 6 anos de experiência como
+                                Projetista elétrico. Já trabalhou com laudos de SPDA, inspeções de vasos de pressão,
+                                projeto de subestações de energia, projetos industriais e comerciais.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6 d-flex">
+                    <div class="member">
+                        <img src="<?php echo BASEURL ?>img/p3.jpg" class="img-fluid" alt="">
+                        <div class="member-content">
+                            <h4>Equipe de Desenvolvimento</h4>
+                            <span>Full Stack Devs</span>
+                            <p>
+                                Profissionais de <br> Análise e Desenvolvimento de Sistemas
+                            </p>
+                            <p align="justify">
+                                Uma equipe formada por profissionais do curso de Análise e Desenvolvimento de Sistemas
+                                da FAEX - Faculdade de Extrema, buscam aprimoramento profissional, pessoal e técnico
+                                diariamente. <br>
+                                Utilizando as mais diversas tecnologias disponíveis no mercado, atuam desenvolvendo os
+                                mais diversos projetos, principalmente voltados para a área de gestão e produção.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <section id="testimonials" class="testimonials">
+        <div class="container">
+
+            <div class="slides-1 swiper" data-aos="fade-up">
+                <div class="swiper-wrapper">
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-item">
+                            <img src="<?php echo BASEURL ?>img/ball-icone.svg" class="testimonial-img" alt="">
+                            <h3>Ball Corporation</h3>
+                            <h4>Embalagens Sustentáveis</h4>
+                            <p>Ball Corporation, líder mundial em embalagens sustentáveis de alumínio.</p>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-item">
+                            <img src="<?php echo BASEURL ?>img/gr-icone.svg" class="testimonial-img" alt="">
+                            <h3>Gente Resultado</h3>
+                            <h4>Consultoria</h4>
+                            <p> Fundada em Extrema/MG, a Consultoria Gente Resultado desenvolve projetos em empresas
+                                privadas e órgãos públicos de todo o Brasil, implementando metodologias e processos que
+                                geram resultados excelentes em velocidade competitiva de mercado.</p>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-item">
+                            <img src="<?php echo BASEURL ?>img/revitale-icone.svg" class="testimonial-img" alt="">
+                            <h3>Revitale</h3>
+                            <h4>Gestão Ambiental e Segurança</h4>
+                            <p>Consultoria especializada em serviços ambientais e segurança do trabalho. Atuação em
+                                atendimento a requisitos legais, eficiência em sistema de tratamento de efluentes,
+                                gestão de resíduos sólidos, ISO 14001, normas regulamentadoras, treinamentos entre todos
+                                os assuntos pertinentes à área ambiental e segurança do trabalho.</p>
+
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-item">
+                            <img src="<?php echo BASEURL ?>img/a3p-icone.svg" class="testimonial-img" alt="">
+                            <h3>A3P Transporte</h3>
+                            <h4>Transporte</h4>
+                            <p>Localizada na cidade de Extrema / MG, a A3P Transporte e Turismo está entre as maiores
+                                empresas do ramo na região. Contando com uma equipe de profissionais altamente
+                                capacitados, frota com mais de 100 veículos e infraestrutura própria a A3P transporte e
+                                Turismo trabalha focada na qualidade e segurança para garantir a melhor prestação de
+                                serviço da região.</p>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="testimonial-item">
+                            <img src="<?php echo BASEURL ?>img/sanox-icone.jpg" class="testimonial-img" alt="">
+                            <h3>Sanox</h3>
+                            <h4>Tratamento de Água</h4>
+                            <p> Com foco no desenvolvimento de processos para identificação de problemas, onde são
+                                analisados os pontos críticos voltados para uma gestão sustentável do tratamento de água
+                                e efluentes.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+
+        </div>
+    </section>
+
+
+    <section id="faq" class="faq">
+        <div class="container" data-aos="fade-up">
+
+            <div class="section-header">
+                <span>Perguntas Frequentes</span>
+                <h2>Perguntas Frequentes</h2>
+
+            </div>
+
+            <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="200">
+                <div class="col-lg-10">
+
+                    <div class="accordion accordion-flush" id="faqlist">
+
+                        <div class="accordion-item">
+                            <h3 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                                    <i class="bi bi-question-circle question-icon"></i>
+                                    Como fazemos em relação a Hospedagem dos Projetos?
+                                </button>
+                            </h3>
+                            <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                <div class="accordion-body">
+                                    Para hospedar os projetos online, nós utilizamos a hospedagem do cliente, caso ele
+                                    já possua alguma. Outra opção é hospedar em nosso servidor dedicado, havendo assim
+                                    uma taxa mensal a ser combinada de acordo com o projeto e suas necessidades.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="accordion-item">
+                            <h3 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                                    <i class="bi bi-question-circle question-icon"></i>
+                                    E a compra de Domínio?
+                                </button>
+                            </h3>
+                            <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                <div class="accordion-body">
+                                    Se ainda não possuir um domínio, nós auxiliamos no processo de aquisição de um,
+                                    sendo o processo todo realizado pelo site oficial registro.br.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="accordion-item">
+                            <h3 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                                    <i class="bi bi-question-circle question-icon"></i>
+                                    Quais as aplicações dos sistemas?
+                                </button>
+                            </h3>
+                            <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                <div class="accordion-body">
+                                    Os nossos sistemas são desenvolvidos de acordo com a necessidade do cliente, portanto, o sistema será desenvolvido para solucionar determinados problemas e demandas.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="accordion-item">
+                            <h3 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
+                                    <i class="bi bi-question-circle question-icon"></i>
+                                    Quais os valores?
+                                </button>
+                            </h3>
+                            <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                <div class="accordion-body">
+                                    <i class="bi bi-question-circle question-icon"></i>
+                                    Como cada projeto tem suas particularidades, não trabalhamos com valores fixos, portanto, um orçamento será realizado após reunião com o cliente e equipe de desenvolvimento.
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="accordion-item">
+                            <h3 class="accordion-header">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
+                                    <i class="bi bi-question-circle question-icon"></i>
+                                    Prazos?
+                                </button>
+                            </h3>
+                            <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
+                                <div class="accordion-body">
+                                    O prazo para o desenvolvimento de um projeto irá variar de acordo com seu tamanho e complexidade. Para todos os projetos, é elaborado um escopo detalhado para seu desenvolvimento. Vale lembrar que quando trabalhado com outras empresas, como por exemplo, de hospedagem, o prazo pode variar devido a comunicação com mais de uma parte.
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+</main>
+
+<style>
+    @media only screen and (max-width: 480px) {
+        #titulo {
+            font-size: 40px;
+        }
+    }
+</style>
---
+++ global_data.php
@@ -0,0 +1,6 @@
+<?php
+
+$qtd_clientes = "11";
+$qtd_projetos = "18";
+$horas_suporte = "237";
+$qtd_colaboradores = "5";
